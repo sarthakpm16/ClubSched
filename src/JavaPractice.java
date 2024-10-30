5e3a0a151d09264d21783aa87dc65a8a975ab77b
sarthakpm16 <sarthakpalmahajan@gmail.com>
2024-10-30 08:14:21 -0400

    practice-gcf

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
 public class JavaPractice {
+   
+    public static int gcf(int a, int b) {
     
-    public static void main(String[] args) {
-        
-
-
-        
-
+    int count = a;
+    if (a  >  b) {
+    count = b;
+    }   
+            while(a % count != 0 || b % count != 0) {
+                count--;
+        } return count;
     }
+        public static void main(String[] args) {
+            gcf(8, 12);
+            gcf(9, 27);
+            gcf(46, 18);
+            gcf(81, 99);
 
-
-
-}
+                }
+    }
