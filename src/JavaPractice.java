b4c3c7695cf6bc90e472908f7fbcd32eac43d9ce
sarthakpm16 <sarthakpalmahajan@gmail.com>
2024-11-11 19:29:57 -0500

    new stuff

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -18,3 +18,6 @@ public class JavaPractice {
 
                 }
     }
+
+
+    public stativ boolean isPrime()
---
+++ src/LoopsInJava.java
@@ -0,0 +1,22 @@
+public class LoopsInJava {
+   /*  public static void odds(int start) {
+        for(int i = start; i>0; i -=2) {
+            for(int a = 0; a < i; a++) {
+                System.out.print(i);
+            } System.out.println();
+        }
+    } */
+
+
+    public static void main(String[] args) {
+        for(int col = 0; col <= 3; col++) {
+            for(int row = 0; row <=5; row++){
+            System.out.println("*");
+            }
+        }
+    }
+}
+
+
+
+}
